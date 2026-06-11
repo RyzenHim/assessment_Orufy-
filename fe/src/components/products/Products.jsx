028fa694b6d6536511cd5d5f9fb81798cefce036
Himanshu <himanshuupadhyay210@gmail.com>
2026-06-11 21:29:35 +0530

    minor fixes

---
+++ fe/src/components/products/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import noItems from '../../assets/Group.png'
+const Products = () => {
+    return (
+        <div>
+
+
+            
+            <img src={noItems} alt='noItmes' />
+            <h1>No Published Products</h1>
+            <p>Your Published Products will appear here
+                Create your first product to publish </p>
+        </div>
+    )
+}
+
+export default Products
